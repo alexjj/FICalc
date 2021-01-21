f7dc973291641f7878733a2ce63ab58b9a7aa9b4
Alex Johnstone <alexjj@gmail.com>
2021-01-21 23:02:00 +0000

    learnin' js

---
+++ app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+// Define variables
+
+let salary = 100000;
+let pension_contribution = 0.05;
+
+alert(`Hello, you pay in £${salary * pension_contribution} to your pension each year!`)
