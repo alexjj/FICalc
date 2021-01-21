0ba098d6ae199ab1e0112b2a569b01c502b3b1de
Alex Johnstone <alexjj@gmail.com>
2021-01-21 23:23:45 +0000

    bit more

--- app.js
+++ app.js
@@ -2,7 +2,7 @@
 
 // Define variables
 
-let salary = 100000;
+let salary = prompt("What's your annual salary?", '100000');
 let pension_contribution = 0.05;
 
 alert(`Hello, you pay in £${salary * pension_contribution} to your pension each year!`)
